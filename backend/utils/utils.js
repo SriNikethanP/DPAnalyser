72b49084409f9739ee330051765072d5c0b6527a
SriNikethanP <srinikethan.ponna@gmail.com>
2025-02-15 14:13:25 +0530

    publishing code

---
+++ backend/utils/utils.js
@@ -0,0 +1,25 @@
+async function fetchGitHubStats(username) {
+  let totalStars = 0;
+  let totalForks = 0;
+  let page = 1;
+  let hasMore = true;
+
+  while (hasMore) {
+    const response = await fetch(
+      `https://api.github.com/users/${username}/repos?per_page=100&page=${page}`
+    );
+    const repos = await response.json();
+
+    if (repos.length === 0) {
+      hasMore = false;
+      break;
+    }
+
+    totalStars += repos.reduce((acc, repo) => acc + repo.stargazers_count, 0);
+    totalForks += repos.reduce((acc, repo) => acc + repo.forks_count, 0);
+
+    page++; 
+  }
+
+  return { totalStars, totalForks };
+}
